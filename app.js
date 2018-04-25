b0a2788563e106336bc486d6b57285b903f8f7f6
KaneMJ <matthewjkane@live.com>
2018-04-24 20:22:52 -0700

    Initial commit to save vidjot

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-development')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+const port = 5000;
+
+// Handlebars Midleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Index Route
+app.get('/', (req, res)=> {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+// About Route
+app.get('/about', (req, res)=> {
+    res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started on port ${port}`);
+});
+
+//Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({text: 'Please add a title'});
+    }
+    if(!req.body.details){
+        errors.push({text: 'Please add some details'});
+    }
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
+});
