9046dd5e1d66287004913e7269a30133dbc84e31
KaneMJ <matthewjkane@live.com>
2018-04-26 08:25:19 -0700

    Finished product, ready to deploy

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ const users = require('./routes/users');
 
 // Passport Config
 require('./config/passport')(passport);
-
+// DB Config
+const db = require('./config/database');
 // Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-development')
+mongoose.connect(db.mongoURI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> {
     console.log(`Server started on port ${port}`);
